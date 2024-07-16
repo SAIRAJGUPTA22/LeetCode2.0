40d99f30543e7108c8d6ab172443634f136e9588
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2024-07-17 00:38:02 +0530

    Time: 38 ms (48.32%) | Memory: 16.4 MB (80.27%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -3,16 +3,16 @@ class Solution:
         if str1+str2 != str2+str1:
             return ""
         from math import gcd
-        if str1+str2 == str1+str2:
-         def gcd(a, b):
-            n1 = max(a, b)
-            n2 = min(a, b)
+        if str1+str2 == str2+str1:
+            def gcd(a, b):
+                n1 = max(a, b)
+                n2 = min(a, b)
     
-            for i in range(n2, 0, -1):
-                if n1 % i == 0 and n2 % i == 0:
-                    return i  # Return the GCD immediately when condition is met
+                for i in range(n2, 0, -1):
+                    if n1 % i == 0 and n2 % i == 0:
+                        return i  # Return the GCD immediately when condition is met
 
-            return 1  # Fallback case; theoretically, 1 is always a divisor
+                return 1  # Fallback case; theoretically, 1 is always a divisor
 
 
         return str1[:gcd(len(str1),len(str2))]
