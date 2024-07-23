ae10b2473f223dea2ad9adb030cab324960c02ff
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2024-07-23 21:17:17 +0530

    Time: 38 ms (68.16%) | Memory: 16.6 MB (17.26%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -4,7 +4,7 @@ class Solution:
             return 0
         first, last =1,x
         while first <= last:
-            mid = first+(last-first)//2
+            mid = (last+first)//2
             if mid == x//mid:
                 return mid
             elif mid > x//mid:
