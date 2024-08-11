8def502c26b0482d493d7ca9eb0537425cd0939c
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2024-08-11 13:06:42 +0530

    Time: 23 ms (99.14%) | Memory: 16.6 MB (45.66%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,19 +1,15 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1+str2 != str2+str1:
-            return ""
-        from math import gcd
+            return ''
         if str1+str2 == str2+str1:
-            def gcd(a, b):
-                n1 = max(a, b)
-                n2 = min(a, b)
-    
-                for i in range(n2, 0, -1):
-                    if n1 % i == 0 and n2 % i == 0:
-                        return i  # Return the GCD immediately when condition is met
+            n1 = max(len(str1),len(str2))
+            n2 = min(len(str1),len(str2))
 
-                return 1  # Fallback case; theoretically, 1 is always a divisor
-
-
-        return str1[:gcd(len(str1),len(str2))]
+            def gcd(n1,n2):
+                for i in range(n2,0,-1):
+                    if n1%i == 0 and n2%i == 0:
+                        return i
+                return 1
+        return  str1[:gcd(n1,n2)]
         
