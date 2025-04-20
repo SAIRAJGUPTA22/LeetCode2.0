06867e4e2ed3543f148d2ef250e06e74321dd952
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-04-20 05:51:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (11.64%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,15 +1,22 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        if x == 0:
-            return 0
-        first, last =1,x
-        while first <= last:
-            mid = (last+first)//2
-            if mid == x//mid:
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x<2:
+            return x
+        left, right = 1, x//2
+        ans = 1
+        while left<=right:
+            mid =(left+right)//2
+            if mid*mid == x:
                 return mid
-            elif mid > x//mid:
-                last = mid-1
+            elif mid*mid < x:
+                ans = mid
+                left = mid+1
             else:
-                first = mid+1
-        return last
+                right = mid-1
+
+        return ans
         
