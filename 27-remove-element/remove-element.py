1a1a7402adc56a40b7e6be8f8d2d8c4e4c7739f5
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-02 09:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (9.81%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,13 +5,12 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        k=0
-        for i in range(len(nums)):
-            if nums[i]  != val:
-                nums[k]=nums[i]
-                k=k+1
- 
+        k = 0
+        for i in range(0,len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k = k+1
+        nums[:k] = sorted(nums[:k])
         for i in range(k,len(nums)):
-            nums[i] ='_'
+            nums[i]='_'
         return k
-        
