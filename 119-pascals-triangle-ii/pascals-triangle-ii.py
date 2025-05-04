b70492830ac62fd7851bbc3042ade762c99c6bf9
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-04 11:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (10.97%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type rowIndex: int
         :rtype: List[int]
         """
-        tri =[]
-        for i in range(rowIndex+1):
-            row =[1]*(i+1)
-            for j in range(1,i):
-                row[j] = tri[i-1][j-1]+tri[i-1][j]
-            tri.append(row)
-        return tri[rowIndex]
+        row = [1]*(rowIndex+1)
+        for i in range(2,rowIndex+1):
+            for j in range(i-1,0,-1):
+                row[j] = row[j]+row[j-1]
+        return row
