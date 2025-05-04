1517d8a558c1e280ebb56da48acd9980be6a0f24
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-04 10:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (21.86%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        triangle =[]
+        triangle=[]
         for i in range(numRows):
             row =[1]*(i+1)
-
             for j in range(1,i):
-                row[j]= triangle[i-1][j-1]+triangle[i-1][j] 
-
+                row[j] = triangle[i-1][j-1]+triangle[i-1][j]
             triangle.append(row)
         return triangle
-        
