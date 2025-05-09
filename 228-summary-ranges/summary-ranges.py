3ca5b7ec8f1746baeeb0a0ab8335f2b973ad690f
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-09 07:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.71%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -4,19 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
-        result =[]
+        result = []
         i = 0
-        n= len(nums)
-
+        n = len(nums)
         while i <n:
             start = i
-            while i+1<n and nums[i+1]==nums[i]+1:
+            while i+1<n and nums[i]+1 == nums[i+1]:
                 i += 1
             if start ==i:
                 result.append(str(nums[i]))
-            else:
-                result.append("{}->{}".format(nums[start],nums[i]))
+            else: 
+                result.append('{}->{}'.format(nums[start],nums[i]))
             i += 1
         return result
-            
-        
