27197c4f5954f5bf42e5f0b575540c9bf9ae2c60
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-24 11:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (25.53%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -5,10 +5,11 @@ class Solution(object):
         :type target: str
         :rtype: str
         """
-        c = ord(target)
-        let = [ord(i) for i in letters]
-        for i in range(len(let)):
-            if let[i] >c:
-                return letters[i]
-        return letters[0]
-        
+        # c = ord(target)
+        # let = [ord(i) for i in letters]
+        # for i in range(len(let)):
+        #     if let[i] >c:
+        #         return letters[i]
+        # return letters[0]
+        c = bisect.bisect_right(letters,target)
+        return letters[c] if c<len(letters) else letters[0]
