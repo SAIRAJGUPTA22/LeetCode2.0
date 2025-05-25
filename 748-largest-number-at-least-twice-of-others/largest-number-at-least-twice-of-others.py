06faab4bfc61f3d5399cf68bd782f52222950bdd
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-25 16:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (48.82%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.py
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.py
@@ -4,13 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = max(nums)
-        c=0
-        for num in nums:
-            if num <>n:
-                if n>= 2*num:
-                    c=c+1
-        if c == len(nums)-1:
-            return nums.index(n)
+        # n = max(nums)
+        # c=0
+        # for num in nums:
+        #     if num <>n:
+        #         if n>= 2*num:
+        #             c=c+1
+        # if c == len(nums)-1:
+        #     return nums.index(n)
+        # return -1
+
+        max_val = -1
+        second_max = -1
+        index = -1
+
+        for i,n in enumerate(nums):
+            if n>max_val:
+                second_max = max_val
+                max_val = n
+                index = i
+            elif n>second_max:
+                second_max = n
+        if max_val >= 2*second_max:
+            return index
         return -1
+
+
         
