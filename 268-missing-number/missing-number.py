86f5ed8f8a2a0d485d040556b3882d61135751cf
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-29 06:29:21 +0530

    Time: 14 ms (35.45%) | Memory: 13.4 MB (30.65%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,7 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n  = len(nums)
-        expected_sum = (n*(n+1))/2
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
+        # n  = len(nums)
+        # expected_sum = (n*(n+1))/2
+        # actual_sum = sum(nums)
+        # return expected_sum - actual_sum
+        left = 0
+        right = len(nums)-1
+        nums.sort()
+        while left <=right:
+            mid =(left+right)//2
+            if nums[mid]==  mid:
+                left = mid+1
+            else: 
+                right = mid-1
+        return left
