530466aa0e914cb514a2a292c7209cc9c82942f5
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-05-29 06:43:33 +0530

    Time: 4 ms (33.23%) | Memory: 12.6 MB (23.14%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -5,5 +5,23 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        return list(set(nums1)&set(nums2))
-        
+        # return list(set(nums1)&set(nums2))
+        def binary_search(arr,target):
+            left =0
+            right = len(arr)-1
+            while left <= right:
+                mid = (left+right)//2
+                if arr[mid]== target:
+                    return True
+                if arr[mid]<target:
+                    left = mid+1
+                else:
+                    right = mid-1
+            return False
+        nums1_unique = list(set(nums1))
+        nums2.sort()
+        result =[]
+        for num in nums1_unique:
+            if binary_search(nums2,num):
+                result.append(num)
+        return result
