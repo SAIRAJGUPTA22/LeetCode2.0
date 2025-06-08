4cedd55d16405e16b191270ae02a62207732e359
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-06-08 18:42:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (80.16%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -4,15 +4,29 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        carry =1 
+        # carry =1 
+        # for i in range(len(digits)-1,-1,-1):
+        #     if digits[i]+carry == 10:
+        #         digits[i] = 0
+        #         carry = 1
+        #     else:
+        #         digits[i] = digits[i] + carry
+        #         carry = 0
+        #         break
+        # if carry ==1:
+        #     digits.insert(0,1)
+        # return digits
+
+        carry = 1
         for i in range(len(digits)-1,-1,-1):
             if digits[i]+carry == 10:
-                digits[i] = 0
+                digits[i]=0
                 carry = 1
             else:
-                digits[i] = digits[i] + carry
+                digits[i]= digits[i]+carry
                 carry = 0
                 break
         if carry ==1:
             digits.insert(0,1)
         return digits
+       
