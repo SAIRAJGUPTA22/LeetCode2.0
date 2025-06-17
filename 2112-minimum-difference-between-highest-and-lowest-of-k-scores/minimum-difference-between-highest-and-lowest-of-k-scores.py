d5eb9cc634bf6f9bf7c95c891914baa0271ee602
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-06-17 18:20:25 +0530

    Time: 7 ms (89.07%) | Memory: 12.6 MB (23.47%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -6,10 +6,10 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        n = len(nums)
+        n=len(nums)
         min_value = float('inf')
+
         for i in range(n-k+1):
             value = nums[i+k-1]-nums[i]
             min_value = min(min_value,value)
         return min_value
-        
