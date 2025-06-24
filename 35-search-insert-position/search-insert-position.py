cd58aa64ee091ec5f150a99f70db0050009b1484
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-06-24 11:32:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (89.61%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         left = 0
         right = len(nums)-1
-        while left <=right:
+        while left<=right:
             mid = (left+right)//2
             if nums[mid] == target:
                 return mid
