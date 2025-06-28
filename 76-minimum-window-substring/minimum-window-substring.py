f5f5b8a6cc4aa1cc7dfde4162316e53c2d7c5a2a
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-06-28 06:26:14 +0530

    Time: 115 ms (58.86%) | Memory: 12.8 MB (61.17%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -5,30 +5,33 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
+        t_freq = Counter(t)
+        left = 0
+        window={}
+        formed = 0
+        req=len(t_freq)
+        min_length= float('inf')
+
+        for right,n in enumerate(s):
+            window[n] = window.get(n,0)+1
+
+            if n in t_freq and window[n]==t_freq[n]:
+                formed += 1
+            while formed == req:
+                window_size = right - left +1
+                if window_size < min_length:
+                    min_length = min(min_length,window_size)
+                    min_left = left 
+                left_char = s[left]
+                window[left_char] = window.get(left_char,0)-1
+                if left_char in t_freq and window[left_char]<t_freq[left_char]:
+                    formed -= 1
+                left +=1
+
+        return "" if min_length == float('inf') else s[min_left:min_left+min_length] 
+
+
 
         
 
-        t_freq = Counter(t)
-        window_freq ={}
-        left = 0
-        min_len = float('inf')
-        min_left = 0
-        formed = 0
-        req = len(t_freq)
-
-        for right,char in enumerate(s):
-            window_freq[char] = window_freq.get(char,0)+1
-            if char in t_freq and window_freq[char] == t_freq[char]:
-                formed +=1
-            while formed == req:
-                window_size = right-left+1
-                if window_size < min_len:
-                    min_len = window_size
-                    min_left = left
-                left_char = s[left]
-                window_freq[left_char] -= 1
-                if left_char in t_freq and window_freq[left_char]<t_freq[left_char]:
-                    formed -=1
-                left +=1
-        return "" if min_len == float('inf') else s[min_left:min_left+min_len]
         
