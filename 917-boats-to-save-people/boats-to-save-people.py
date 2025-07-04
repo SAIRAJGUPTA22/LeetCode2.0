638511c2b1879cc442c1dd52b1a676dd8788dfb6
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-07-04 18:48:56 +0530

    Time: 64 ms (96.19%) | Memory: 16.5 MB (47.76%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        """
+        :type people: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        people.sort()
+        boat=0
+        left = 0
+        
+        n = len(people)
+        right = n-1
+        while left<=right:
+            if people[left]+people[right]<=limit:
+                left +=1
+            right -=1
+            boat +=1
+        return boat
