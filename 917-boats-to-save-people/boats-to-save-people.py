6a441d83fbe4b80ec5b37c7dd9fdc44c7c5ba1b8
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-07-07 15:57:46 +0530

    Time: 71 ms (58.75%) | Memory: 16.4 MB (77.23%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -6,11 +6,11 @@ class Solution(object):
         :rtype: int
         """
         people.sort()
-        boat=0
+        boat = 0
         left = 0
-        
         n = len(people)
         right = n-1
+
         while left<=right:
             if people[left]+people[right]<=limit:
                 left +=1
