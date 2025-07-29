b64a28a8ab02ea5b4f4ad1836ca6731d031c2806
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-07-29 10:01:00 +0530

    Time: 4 ms (45.82%) | Memory: 13.9 MB (58.14%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -3,21 +3,12 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
-        # """
-        # k= 1
-        # for i in range(1,len(nums)):
-        #     if nums[i] != nums[i-1]:
-        #         nums[k] = nums[i]
-        #         k = k+1
-        # for i in range(k,len(nums)):
-        #     nums[i] ='_'
-        # return k
-
+        """
         k = 1
         for i in range(1,len(nums)):
             if nums[i] != nums[i-1]:
                 nums[k] = nums[i]
-                k = k+1
+                k += 1
         for i in range(k,len(nums)):
-            nums[i] ="_"
+            nums[i]='_'
         return k
