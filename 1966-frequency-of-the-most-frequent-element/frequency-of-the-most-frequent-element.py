19857a1abbe69bd43f194587211b0d6fca486d1c
SAIRAJGUPTA22 <94795635+SAIRAJGUPTA22@users.noreply.github.com>
2025-08-12 12:51:42 +0530

    Time: 410 ms (6.13%) | Memory: 21.1 MB (29.86%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.py
@@ -5,23 +5,23 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-
         left = 0
-        max_l =0
-        total =  0
+        total = 0
+        max_l = 0
         window_size = 0
         nums.sort()
 
+
         for right in range(len(nums)):
-            total += nums[right]
             window_size = right - left +1
-            operations = window_size*(nums[right])-total
-            while operations>k:
+            total += nums[right]
+            operations = nums[right]*window_size - total
+
+            while operations >k:
                 total -= nums[left]
-                left += 1
+                left +=1
                 window_size = right -left+1
-                operations = window_size*(nums[right])-total
+                operations = (nums[right]*window_size) - total
             if operations <=k:
-                max_l = max(window_size,max_l)
-        
+                max_l = max(max_l,right-left+1)
         return max_l
